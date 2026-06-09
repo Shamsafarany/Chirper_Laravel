d953cec89f76b17d78fc6779e31702f16ac17be0
Sham Safarany <125689014+Shamsafarany@users.noreply.github.com>
2026-06-09 13:18:51 +0300

    add routes and blades

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -18,7 +18,7 @@
             <a href="/" class="btn btn-ghost text-xl">🐦 Chirper</a>
         </div>
         <div class="navbar-end gap-2">
-            <a href="#" class="btn btn-ghost btn-sm">Sign In</a>
+            <a href="../signIn" class="btn btn-ghost btn-sm">Sign In</a>
             <a href="#" class="btn btn-primary btn-sm">Sign Up</a>
         </div>
     </nav>
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,11 @@
 <?php
 
+use App\Http\Controllers\ChirpController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('home');
+Route::get('/', [ChirpController::class, 'index']);
+
+Route::get('/signIn', function(){
+    return view('signIn', ["username" => "example@gmail.com"]);
 });
+
