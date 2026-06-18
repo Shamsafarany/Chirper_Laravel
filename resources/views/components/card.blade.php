55584964aebb109bbab824527df922e251ce3fd8
Sham Safarany <125689014+Shamsafarany@users.noreply.github.com>
2026-06-18 13:57:26 +0300

    add card component: " " clear exit q

--- resources/views/components/card.blade.php
+++ resources/views/components/card.blade.php
@@ -1,3 +1,29 @@
-<div>
-    
+@props(['chirp'])
+
+<div class="card bg-base-100 shadow mt-8">
+    <div class="card-body border-rounded">
+        <div class="flex space-x-3">
+            @if ($chirp->user)
+                <div class="avatar">
+                    <div class="size-10 rounded-full">
+                        <img src="https://avatars.laravel.cloud/{{ urlencode($chirp->user->email) }}" alt="{{ $chirp->user->name }}'s avatar" class="rounded-full">
+                    </div>
+                </div>
+            @else
+                <div class="avatar placeholder">
+                    <div class="size-10 rounded-full">
+                        <img src="https://avatars.laravel.cloud/f61123d5-0b27-434c-a4ae-c653c7fc9ed6?vibe=stealth" alt="Anonymous User" class="rounded-full">
+                    </div>
+                </div>
+            @endif
+            <div>
+                <div class="flex items-center space-x-1">
+                <p class="font-semibold">{{$chirp->user ? $chirp->user->name: 'Anonymous' }}</p>
+                <span class="text-based-content/60">.</span>
+                <p class="text-sm text-base-content/60">{{ $chirp->created_at->diffForHumans() }}</p>
+            </div>
+            <p class="mt-1">{{ $chirp->message }}</p>
+            </div>
+        </div>
+    </div>
 </div>
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,28 +2,22 @@
     <x-slot:title>
         Home
     </x-slot:title>
-      
+
     <div class="max-w-2xl mx-auto">
-       @forelse ($chirps as $chirp)
+        <h1 class="font-bold text-3xl mt-8">Latest Chirps</h1>
+        @forelse ($chirps as $chirp)
+            <x-card :chirp="$chirp"/>
+        @empty
             <div class="card bg-base-100 shadow mt-8">
                 <div class="card-body">
-                    <div>
-                        <div class="font-semibold">{{$chirp->user ? $chirp->user->email: 'Anonymous' }}</div>
-                        <div class="mt-1">{{ $chirp->message }}</div>
-                        <div class="text-sm text-gray-500 mt-2">{{ $chirp->created_at->diffForHumans() }}</div>
-                    </div>
-                </div> 
-            </div>  
-       @empty
-            <div class="card bg-base-100 shadow mt-8">
-                <div class="card-body">
-                      No Chirps Exist Yet!
+                    No Chirps Exist Yet!
                 </div> 
             </div>
-       @endforelse
+        @endforelse
     </div>
     
     <x-slot:year>
         {{ date('Y') }}
     </x-slot:year>
 </x-layout>
+
